9a42b2c96e9b688e69f73ba73066f3fef198d405
marve123 <eric.marvelboy@gmail.com>
2024-05-30 05:45:37 +0100

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Stream",
+  description: "Video Conferencing App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
